2e092c58890e7fa40f20be0274329e9b96ef8514
Brittany Hilliard <bnicholehilliard@gmail.com>
2022-03-21 19:14:48 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import axios from "axios"; 
+
 // 👉 TASK 1- Test out the following endpoints:
 
 //  https://dog.ceo/api/breeds/image/random
@@ -8,17 +10,36 @@
 
 // 👉 TASK 2- Select the "entry point", the element
 // inside of which we'll inject our dog cards 
-const entryPoint = null
+const entryPoint = document.querySelector(".entry");
 
 
 // 👉 TASK 3- `dogCardMaker` takes an object and returns a Dog Card.
 // Use this function to build a Card, and append it to the entry point.
 function dogCardMaker({ imageURL, breed }) {
   // instantiating the elements
+
+  const dogCard = document.createElement("div");
+  const dogImg = document.createElement("img");
+  const heading = document.createElement("h3");
+
+dogCard.classList.add("dog-card");
+dogImg.classList.add("dog-image")
+
+heading.textContent = `Breed: ${breed}`;
+dogImg.src = imageURL;  
+
+  dogCard.appendChild(dogImg);
+  dogCard.appendChild(heading);
+
+dogCard.addEventListener("click", () => {
+  dogCard.classList.toggle("selected");
+})
+
+return dogCard;
   /*
     <div class="dog-card">
       <img class="dog-image">
-      <h3>
+      <h3></h3>
     </div>
   */
   // set class names, attributes and text
@@ -27,13 +48,28 @@ function dogCardMaker({ imageURL, breed }) {
 
   // add some interactivity
 
+
   // never forget to return!
 }
+
+dogCardMaker({imageURL: "Blah.com", breed: "ugh"});
 
 
 // 👉 TASK 4- Bring the Axios library into the project using one of two methods:
 //    * Traditional way: put another script tag inside index.html (`https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js`)
 //    * Projects with npm: install it with npm and import it into this file
+
+axios.get("https://dog.ceo/api/breed/akita/images/random/2")
+.then(responseVariable => {
+console.log(responseVariable.data.message);
+})
+.catch(error => {
+  console.error(error);
+})
+.finally( () => {
+  console.log("it's working");
+})
+
 
 
 // 👉 TASK 5- Fetch dogs from `https://dog.ceo/api/breed/{breed}/images/random/{number}`
